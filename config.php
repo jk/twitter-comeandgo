21b4b87703a69456f99bad36c5ad9b3308c6c324
Jens Kohl <jens.kohl@gmail.com>
2008-12-13 21:50:57 +0100

    In die README einen weiteren Hinweis über hardcodierte Infos in der index.php hinzugefügt. Ausserdem wurde der GOOGLE_ANALYTICS_ID String auf leer gesetzt, so dass das standardmäßig keine Statistiken über die Besucher erhoben werden.

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ define('PASSWD', 'password');
 define('DIR', '/webserver/docs/twitter');
 define('ORDER', 'older');
 define('DAYS', 14);
-define('GOOGLE_ANALYTICS_ID', 'UA-#####-#');
+define('GOOGLE_ANALYTICS_ID', ''); # UA-#####-#
 ?>
